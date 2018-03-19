bdaf4a165c23902ef1c015dfbf98501bd2faba87
skonhed <37516980+skonhed@users.noreply.github.com>
2018-03-19 22:36:40 +0300

    Add files via upload

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-type: text/html; charset=utf-8');
+$wall_id="-114238095"; // Положительное число: пользователь. Отрицательное: группа.
+$count="100"; // Количество записей, которое необходимо получить. Максимальное значение: 100. 
+$api = file_get_contents("http://api.vk.com/method/wall.get?owner_id={$wall_id}&count={$count}");
+$wall = json_decode($api,true);
+foreach ($wall['response'] as $item => $result) {
+  echo $result['text'].'<hr/>';
+}
+?>
